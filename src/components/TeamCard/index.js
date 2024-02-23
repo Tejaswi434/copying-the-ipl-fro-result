232e5877bc3235009713cfd34dca62b2f47aa82c
Tejaswi434 <tejatango301203@gmail.com>
2024-02-23 10:54:34 +0000

    jjhjhj

---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+import {Link} from 'react-router-dom'
+
+const TeamCard = props => {
+  const {teamDetails} = props
+  const {name, id, teamImageURL} = teamDetails
+
+  return (
+    <li className="team-item">
+      <Link to={`/team-matches/${id}`} className="link-item">
+        <img src={teamImageURL} alt={name} className="team-logo" />
+        <p className="team-name">{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default TeamCard
